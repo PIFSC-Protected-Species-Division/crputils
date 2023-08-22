4d7d4dbad8ebfaf9a30e0f8e9b64f623c29c5706
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-08-21 17:28:17 -0700

    mod to read csv directly, optional dates/times

--- R/csvToGPX_tracks.R
+++ R/csvToGPX_tracks.R
@@ -1,115 +1,156 @@
-trackToGPX = function(tdf, outGPX){
-  #' trackToGPX
-  #' 
-  #' description: convert a .csv of vessel track segments to a .gpx file. 
-  #' 
-  #' The input data.frame must have 1 column 'uid' containing a unique 
-  #' identifier for that track/segment, 2 columns for start latitude and 
+CSVToGPX_track = function(inCSV, outGPX){
+  #' CSVToGPX_track
+  #'
+  #' description: convert a .csv of vessel track segments to a .gpx file.
+  #'
+  #' The input data.frame must have 1 column 'uid' containing a unique
+  #' identifier for that track/segment, 2 columns for start latitude and
   #' longitude, and 2 columns for end latitude and longitude.
   #' Additional columns for cruise, date, vessel name, etc will be ignored
-  #' 
-  #' This is a modified version of `trackToGPX.R` that is included in the 
-  #' 'cruise-maps-live' repository that tries to generalize the function to 
+  #'
+  #' This is a modified version of `trackToGPX.R` that is included in the
+  #' 'cruise-maps-live' repository that tries to generalize the function to
   #' convert any input data.frame, not just the output from a .das file.
-  #' 
-  #' This requires the lubridate package. Most people have this installed as 
-  #' part of the tidyverse packages, but if not, install using 
-  #' install.packages('lubridate') 
-  #' 
+  #'
+  #' This requires the lubridate package. Most people have this installed as
+  #' part of the tidyverse packages, but if not, install using
+  #' install.packages('lubridate')
+  #'
   #' author: Selene Fregosi selene.fregosi [at] noaa.gov
   #' date: 28 July 2023
   #'
-  #' @param tdf data.frame of track segments. Must include the following 
-  #' columns: uid, lat1, lon1, lat2, lon2
+  #' @param inCSV filename of csv containing track data to be processed. Must
+  #' include the following columns:
+  #' uid, lat1, lon1, lat2, lon2
+  #' where uid is a unique track identifier, lat/lon1 is the start location and
+  #' lat/lon2 is the end location.
+  #' All locations should be in decimal degrees.
+  #' Can optionally include a dateTime1 and dateTime2 column, or a single date
+  #' or dateTime column. Any additional columns will be ignored.
   #' @param outGPX fullpath filename of gpx to be written
-  #' example: outGPX = './CRPTools/utils-R/exampleData/exampleVesselTrack.gpx'
-  #' 
+  #' example: outGPX = './crputils/exampleData/exampleVesselTrack.gpx'
+  #'
   #' @return none, will write a file
   #' @examples
-  #' tdf = read.csv('./CRPTools/utils-R/exampleData/exampleVesselTrack.csv')
-  #'  
+  #' inCSV = './exampleData/exampleVesselTrack.csv'
+  #' outGPX = './exampleData/exampleVesselTrack.gpx'
+  #' CSVToGPX(inCSV, outGPX)
+  #'
   #' ######################################################################
   #'
-  
+
+  # read in CSV
+  tdf = read.csv(inCSV)
   # Coerce tdf (track data frame) into a simplified longform format
-  
+
   # trim to just essential columns
-  tt = subset(tdf, select = c(uid, lat1, lon1, lat2, lon2))
+  trimCols = c('uid', 'lat1', 'lon1', 'lat2', 'lon2')
+  # check if dateTime columns exist
+  if ('date' %in% colnames(tdf)){
+    trimCols = c(trimCols, 'date')
+  }
+  if ('dateTime' %in% colnames(tdf)){
+    trimCols = c(trimCols, 'dateTime')
+  }
+  if ('dateTime1' %in% colnames(tdf)){
+    trimCols = c(trimCols, 'dateTime1')
+  }
+  if ('dateTime2' %in% colnames(tdf)){
+    trimCols = c(trimCols, 'dateTime2')
+  }
+  tt = subset(tdf, select = trimCols)
 
   # if any segnums are duplicated, print error message
   if (any(duplicated(tt$uid))){
     stop('Warning: duplicate unique identifiers. Correct in .csv and try again')
   }
-  
+
   # reshape to long format with 'status' tag for start or end of each segment
+  # if dateTime1 and 2 are present, include that
+  varyList = list(lat = c('lat1', 'lat2'), lon = c('lon1', 'lon2'))
+  varyNames = c('lat', 'lon')
+  if ('dateTime1' %in% colnames(tt) && 'dateTime2' %in% colnames(tt)){
+    varyList$dt = c('dateTime1', 'dateTime2')
+    varyNames = c(varyNames, 'dateTime')
+  } else if ('dateTime' %in% colnames(tt)){
+    varyList$dt = c('dateTime')
+    varyNames = c(varyNames, 'dateTime')
+  }else if ( 'date' %in% colnames(tt)){
+    varyList$dt = c('date')
+    varyNames = c(varyNames, 'date')
+  }
+
   ttl = reshape(tt, direction = 'long',
                 idvar = 'uid',
                 ids = tt$uid,
-                varying = list(lat = c('lat1', 'lat2'),
-                               lon = c('lon1', 'lon2')),
-                v.names = c('lat', 'lon'),
+                varying = varyList,
+                v.names = varyNames,
                 timevar = 'status',
                 times = c('start', 'end'))
-  
+
   # reorder by uid
   ttl = ttl[order(ttl$uid),]
-  
+
   # set up the GPX file header info
   # this is copied from a gpx file made using an online converter
   gpxHeader_xmlVer = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>';
   # The websites and such might not be necessary...
-  gpxHeader_gpxVer = paste('<gpx version="1.1" creator="R trackToGPX.R"', 
+  gpxHeader_gpxVer = paste('<gpx version="1.1" creator="R CSVToGPX.R"',
                            'xmlns="http://www.topografix.com/GPX/1/1"',
                            'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"',
                            'xsi:schemaLocation="http://www.topografix.com/GPX/1/1',
                            'http://www.topografix.com/GPX/1/1/gpx.xsd">')
-  # metadata - other things could be added here.  
-  gpxHeader_metadata = paste0('<metadata><bounds ', 
-                              'minlat="', format(min(ttl$lat), nsmall = 6), '" ', 
+  # metadata - other things could be added here.
+  gpxHeader_metadata = paste0('<metadata><bounds ',
+                              'minlat="', format(min(ttl$lat), nsmall = 6), '" ',
                               'minlon="', format(min(ttl$lon), nsmall = 6), '" ',
                               'maxlat="', format(max(ttl$lat), nsmall = 6), '" ',
                               'maxlon="', format(max(ttl$lon), nsmall = 6), '"',
                               '/></metadata>')
-  
-  
-  
+
+
+
   # start assembling the file contents, in 'o'
-  o = c(gpxHeader_xmlVer, 
-        gpxHeader_gpxVer, 
+  o = c(gpxHeader_xmlVer,
+        gpxHeader_gpxVer,
         gpxHeader_metadata
   )
-  
+
   # loop through all uids
   uidList = unique(ttl$uid)
-  
-  # set up the day (track)
+
+  # Each UID = one track made of one track segment
   for (u in 1:length(uidList)){
-uid = uidList[u]
+    uid = uidList[u]
     uIdx = which(ttl$uid == uid)
-    # add to our output string
-    o = c(o, '<trk>', 
+    # add to output string
+    o = c(o, '<trk>',
           paste0('  <name>', uidList[u], '</name>'),
           '  <trkseg>')
-    
+
     for (j in 1:2){
-      o = c(o, 
-            paste0('    <trkpt lat="', ttl$lat[uIdx][j], 
-                   '" lon="', ttl$lon[uIdx][j],'"></trkpt>')
-            )
+      o = c(o,
+            paste0('    <trkpt lat="', ttl$lat[uIdx][j],
+                   '" lon="', ttl$lon[uIdx][j],'">')
+      )
+      if ('dateTime' %in% colnames(ttl)){
+        o = c(o, paste0('      <time>', etLong$dt[dtIdx][segIdx][j],'</time>'))
+      }
+      o = c(o, '    </trkpt>')
     }
     o = c(o,
-          '  <trkseg>', 
-          '</trk>' 
-          )
-   
+          '  <trkseg>',
+          '</trk>'
+    )
+
   }
-  
-  # wrap up the file 
+
+  # wrap up the file
   o = c(o, '</gpx>')
-  
+
   # open the file, write it, and close it
   fileConn = file(outGPX)
   writeLines(o, fileConn)
   close(fileConn)
-  
+
 }
