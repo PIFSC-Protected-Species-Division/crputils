e8978cea7c8453f249e5c62e339ba3001d84e7c6
sfregosi <22456126+sfregosi@users.noreply.github.com>
2024-04-17 10:28:44 -0700

    created extractDASTrack from extractTrack
    from cruise-maps-live

---
+++ R/extractDASTrack.R
@@ -0,0 +1,47 @@
+#' extractDASTrack
+#'
+#' @description Pull effort tracks from a relatively raw daily .das file into
+#' a simpler dataframe. Utilizes the package 'swfscDAS' and then cleans up those
+#' outputs a bit. Generalized from cruise-maps-live's extractTrack()
+#'
+#' author: Selene Fregosi selene.fregosi [at] noaa.gov
+#' last updated: 17 April 2024
+#'
+#' @param df_proc processed das file (with swfscDAS::das_process)
+#'
+#' @return a dataframe of effort tracks with date and lat/lon
+#' @export
+#'
+#' @examples
+#' # extract all new tracks from a given das file, d$name
+#' et = extractTrack(here('inputs', d$name))
+#'
+
+extractDASTrack <- function(df_proc){
+
+  # summarize effort segments. 'section' method pulls lat/lon for all 'R' (resume
+  # effort) and all 'E' (end effort) entries, then calcs dist btwn
+  if (any(df_proc$Event == 'R')){ # check for any on effort segments
+
+    et_all = swfscDAS::das_effort(df_proc, method = 'section', dist.method = 'greatcircle',
+                                  num.cores = 1)
+    # trim to just what we want
+    et_seg = et_all$segdata
+    et = subset(et_seg, select = c(Cruise, segnum, file, stlin:mtime, Mode,
+                                   EffType, avgSpdKt, avgBft))
+
+    # rename the file_das column to match tracks output
+    colnames(et)[3] = 'file_das'
+
+    # effort types can be 'N' non-standard, 'S' standard', and 'F' fine-scale
+    # could further trim by this.
+
+
+
+  } else {
+    et = NULL
+  }
+
+  return(et)
+
+}
