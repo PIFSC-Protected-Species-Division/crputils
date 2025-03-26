ef067aefbab473343f226845cb71cb688711c3b8
selenefregosi-noaa <selene.fregosi@noaa.gov>
2025-03-26 16:34:22 -0700

    added lldistkm function

--- R/downloadGoogleDriveFolder.R
+++ R/downloadGoogleDriveFolder.R
@@ -1,33 +1,33 @@
 #' downloadGoogleDriveFolder
 #'
-#' @description recursively download all files and subdirectories in a google 
+#' @description recursively download all files and subdirectories in a google
 #' drive folder
 #'
 #' @author Selene Fregosi
 #'
 #' @param folder_id Google Drive folder ID, parsed as the 'id' column finding
-#'                  the drive info using googleddrive::drive_get or using 
-#'                  using googledrive::as_id and the folder URL 
-#' @param local_path full path to the folder to save the downloaded files                 
+#'                  the drive info using googleddrive::drive_get or using
+#'                  using googledrive::as_id and the folder URL
+#' @param local_path full path to the folder to save the downloaded files
 #'
 #' @export
-#' @import googledrive drive_ls drive_download
+#' @importFrom googledrive drive_ls drive_download
 #'
 #' @examples
-#' 
+#'
 
 # Define the recursive download function
 downloadGoogleDriveFolder <- function(folder_id, local_path) {
   # Create the local directory if it doesn't exist
   dir.create(local_path, showWarnings = FALSE, recursive = TRUE)
-  
+
   # List all items in the folder
-  items <- googledrive::drive_ls(as_id(folder_id))
-  
+  items <- googledrive::drive_ls(googledrive::as_id(folder_id))
+
   # Loop through each item
   for (i in seq_len(nrow(items))) {
     item <- items[i, ]
-    
+
     # Check if the item is a folder
     if (item$drive_resource[[1]]$mimeType == "application/vnd.google-apps.folder") {
       # If it's a folder, recursively download its contents
---
+++ R/lldistkm.R
@@ -0,0 +1,75 @@
+#' Calculate distance in km between to lat/lons
+#'
+#' @description Utility to calculate the distance (in kilometers) between a pair
+#' of latitude/longitude coordinates. It returns two distances: d1km which is
+#' calculated using the Haversine formula which accounts for the shape of the
+#' earth and d2km based on Pythagoras' theorem and is not accurate over long
+#' distances. Over short distances d1km and d2km will be very similar.
+#'
+#' @author Selene Fregosi
+#'
+#' @param latlon1 1-by-2 matrix of lat/lon coordinates (in decimal degrees)
+#' e.g., \code{matrix(c(30, 29.2020), nrow = 1, ncol = 2)}\cr
+#' @param latlon2 1-by-2 matrix of lat/lon coordinates (in decimal degrees)
+#' e.g., \code{matrix(c(30, 29.2020), nrow = 1, ncol = 2)}\cr
+#'
+#' @returns list with d1km (distance calculated by haversine formula) and d2km
+#' (distance calculated based on Pythagorean theorem)
+#'
+#' @export
+#'
+#' @examples
+#' #short distance
+#' #create input coordinates
+#' ll1 <- c(21, -157)
+#' ll2 <- c(22, -158)
+#' #calculate distance
+#' dkm <- lldistkm(ll1, ll2)
+#' dkm
+#' # $d1km
+#' # [1] 151.8795
+#' #
+#' # $d2km
+#' # [1] 151.8809
+#'
+#' #long distance
+#' #create input coordinates
+#' ll1 <- c(21, -157)
+#' ll2 <- c(13, 144)
+#' #calculate distance
+#' dkm <- lldistkm(ll1, ll2)
+#' dkm
+#' # $d1km
+#' # [1] 6304.003
+#' #
+#' # $d2km
+#' # [1] 32019.57
+#' #d2km not acccurate at longer distances
+
+lldistkm <- function(latlon1, latlon2){
+
+  radius <- 6371 # radius of the earth
+
+  #separate and convert to radians
+  lat1 <- latlon1[1]*pi/180
+  lat2 <- latlon2[1]*pi/180
+  lon1 <- latlon1[2]*pi/180
+  lon2 <- latlon2[2]*pi/180
+
+  #find difference
+  deltaLat <- lat2-lat1
+  deltaLon <- lon2-lon1
+
+  #Haversine distance
+  a <- sin((deltaLat)/2)^2 + cos(lat1)*cos(lat2) * sin(deltaLon/2)^2
+  c <- 2*atan2(sqrt(a), sqrt(1-a))
+  d1km <- radius*c
+
+  #Pythagorean distance
+  x <- deltaLon*cos((lat1+lat2)/2)
+  y <- deltaLat
+  d2km <- radius*sqrt(x*x + y*y)
+
+  dkm <- list(d1km = d1km, d2km = d2km)
+
+}
